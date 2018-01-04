772a4808cd655db8f3ae91820e34138b63e97ea1
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-01-03 18:46:04 -0600

    Update and rename sendMorningEmail.sh to sendDataEmail.sh

---
+++ sendDataEmail.sh
@@ -0,0 +1,18 @@
+# This code sends the data email
+
+# Get to the repository directory where the python code resides
+cd ~/raspi_python
+
+# Call the python to generate the text of the email and store it in a temp file
+python3 -c 'import makeMorningEmail; makeMorningEmail.main()' > tempMessage.txt
+
+# Log the date/time of the message and add the message to the log
+date +'%Y-%m-%d %H:%M' >> /home/pi/logging/data_email_log.txt
+tempMessage.txt >> /home/pi/logging/data_email_log.txt
+
+# Send the email
+cat tempMessage.txt | mail -s "Morning Data" mot5600@yahoo.com
+
+# Remove the temp file and go back to the home directory
+rm tempMessage.txt
+cd ~
