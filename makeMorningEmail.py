4e43baad8b5d9e6322af1fc3be2de66b575d408c
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-01-06 10:46:19 -0600

    Update makeMorningEmail.py

--- makeMorningEmail.py
+++ makeMorningEmail.py
@@ -58,34 +58,71 @@ def getMoonPhaseMessage():
 def getWeightData():
     # Get the API keys from where I've stored them locally on the pi
     fitbit_api_keys = json.load(open("/home/pi/keys/fitbit_api_keys.json"))
-    clientID = fitbit_api_keys['client_id']
-    clientSecret = fitbit_api_keys['client_secret']
-    token = fitbit_api_keys['access_token']
+    client_id = fitbit_api_keys['client_id']
+    client_secret = fitbit_api_keys['client_secret']
+    access_token = fitbit_api_keys['access_token']
     refresh_token = fitbit_api_keys['refresh_token']
     
-    # first bit of code here should use the refresh token to get new tokens
+    # This code refreshes the access token
+    refresh_err, a, r = refreshFitbitTokens(client_id, client_secret, refresh_token)
+    if not refresh_err:
+        fitbit_api_keys['access_token'] = a
+        fitbit_api_keys['refresh_token'] = r
+        fitbit_api_keys['last-refreshed'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        access_token = a
+        refresh_token = r
+    else: 
+        return
     
     # next bit will declare the client, and get data
-    auth_client = fitbit.Fitbit(clientID, clientSecret, access_token=token, refresh_token=refresh_token)
+    auth_client = fitbit.Fitbit(client_id, client_secret, access_token=access_token, refresh_token=refresh_token)
     weight_data = auth_client.get_bodyweight(period='30d')
     
     # last bit will do the moving average calculation and format the message
     no_obs = len(weight_data['weight'])
     for i in range(no_obs):
-        weight_array[i] = weight_data['weight'][i]['weight']
-
-    return " "
-
-def getNewFitbitAccessToken(clientID, clientSecret, refresh_tokey):
-    TokenURL = "https://api.fitbit.com/oauth2/token"
-    BodyText = {'grant_type':'refresh_token',
-                'refresh_token':refresh_tokey}
-    BodyURLEncoded = urllib.parse.urlencode(BodyText)
-    tokenreq = urllib2.request.Request(TokenURL,BodyURLEncoded)
+        weight_array.append(weight_data['weight'][i]['weight'])
     
-    # This next line isn't quite working as expected...
-    tokenreq.add_header('Authorization', 'Basic ' + base64.b64encode(clientID + ":" + clientSecret))
+    weight_avg = round(sum(weight_array)/len(weight_array),1)
+    weight_array.clear()
+    return weight_avg
 
+def refreshFitbitTokens(client_id, client_secret, refresh_token):
+    TokenURL = 'https://api.fitbit.com/oauth2/token'
+    BodyText = {'grant_type':'refresh_token',
+                'refresh_token':refresh_token}
+    BodyURLEncoded = urllib.parse.urlencode(BodyText).encode('utf-8')
+    tokenreq = urllib.request.Request(TokenURL,BodyURLEncoded)
+    
+    # IT WORKS NOW.  DEAR LORD YES
+    tokenreq.add_header('Authorization', 'Basic ' + base64.b64encode(bytes(client_id + ":" + client_secret, 'utf-8')).decode('utf-8'))
+    tokenreq.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    
+    try:
+        tokenresponse = urllib.request.urlopen(tokenreq)
+
+        #See what we got back.  If it's this part of  the code it was OK
+        FullResponse = tokenresponse.read()
+
+        #Need to pick out the access token and write it to the config file.  Use a JSON manipluation module
+        ResponseJSON = json.loads(FullResponse.decode('utf-8'))
+
+        #Read the access token as a string
+        NewAccessToken = str(ResponseJSON['access_token'])
+        NewRefreshToken = str(ResponseJSON['refresh_token'])
+        refresh_err = False
+
+    except urllib2.URLError as e:
+        # Getting to this part of the code means we got an error
+        # print e.code
+        # print e.read()
+        NewAccessToken = ''
+        NewRefreshToken = ''
+        refresh_err = True
+        
+    return refresh_err, NewAccessToken, NewRefreshToken
+        
+    
 def main():
     b, b2 = getBTCprice()
     a, a2 = get538trumpapprove()
