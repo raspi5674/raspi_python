bbf737acf0a1702c0079fb3d44f0c7a279aa13c0
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-01-06 14:00:06 -0600

    Update sendDataEmail.sh

--- sendDataEmail.sh
+++ sendDataEmail.sh
@@ -6,12 +6,6 @@ cd ~/raspi_python
 # Call the python to generate the text of the email and store it in a temp file
 python3 -c 'import makeDataEmail; print(makeDataEmail.main())' > tempMessage.txt
 
-# THIS CODE NOW DONE IN PYTHON
-# Log the date/time of the message and add the message to the log
-#date +'%Y-%m-%d %H:%M' >> /home/pi/logging/data_email_log.txt
-#cat tempMessage.txt >> /home/pi/logging/data_email_log.txt
-#echo -en '\n' >> /home/pi/logging/data_email_log.txt
-
 # Send the email
 cat tempMessage.txt | mail -s "Daily Data" mot5600@yahoo.com
 
