f0fef4fee28087485d3d0e6a93a12c254385d437
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-07-27 12:01:22 -0500

    Adding comments with test code

--- makeDataEmail.py
+++ makeDataEmail.py
@@ -122,9 +122,21 @@ def updateWeightDatabase():
           bf = 'NULL'
        else:
           bf = round(weight_data[i]['fat'],1)
-
+       
+       # THIS IS BROKEN CURRENTLY
        cur.execute("UPDATE weight SET weight=%s, bf_pcnt=%s WHERE date='%s';" % (wt,bf,dt))
-
+       
+       # NEW TEST CODE
+       # MAY NEED TO UPGRADE TO SQLITE 3.24 TO USE THIS CODE
+       # wt_updt = ""
+       # bf_updt = ""
+       # if wt != 'NULL':
+       #    wt_updt = "weight=%s " % wt
+       # if bf != 'NULL':
+       #    bf_updt = "bf_pcnt=%s " % bf
+       # upsert = "INSERT INTO weight VALUES ('%s',%s,%s) ON CONFLICT(date) DO UPDATE SET %s%sWHERE date='%s';" % (dt,wt,bf,wt_updt,bf_updt,dt)
+       # cur.execute(upsert)
+  
     conn.commit()
     cur.close()
     conn.close()
