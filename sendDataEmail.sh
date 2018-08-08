f5d6d1e47d743d29ea0e7052899490b80362b301
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-08-07 23:12:48 -0500

    parameter changes

--- sendDataEmail.sh
+++ sendDataEmail.sh
@@ -4,7 +4,7 @@
 cd ~/raspi_python
 
 # Call the python to generate the text of the email and store it in a temp file
-python3 -c 'import makeDataEmail, os; print(makeDataEmail.main(os.getcwd(), True))' > tempMessage.txt
+python3 -c 'import makeDataEmail; print(makeDataEmail.main(True))' > tempMessage.txt
 
 # Send the email
 cat tempMessage.txt | mail -s "Daily Data" mot5600@yahoo.com -A daily_img.png
