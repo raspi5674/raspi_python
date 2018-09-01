6d57f3b517b81db6126a0f05dfffae6d6f33bd19
raspi5674 <34964197+raspi5674@users.noreply.github.com>
2018-09-01 09:58:44 -0500

    Update and rename lexiMessages.py to Messages.py

--- Messages.py
+++ Messages.py
@@ -1,6 +1,6 @@
 import random, pandas
 
-def getRandomLexiMessage():
+def getRandomMessage():
     messages = pandas.read_csv(messages.csv)
     rand_message = messages['morning'].values(random.randint(0,len(messages)))
     return rand_message
