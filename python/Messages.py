7a9ad147220172954c94822d400244452afc73fd
raspi5674 <raspi5674@gmail.com>
2020-12-27 10:47:05 -0600

    removing old files

--- python/Messages.py
+++
@@ -1,6 +0,0 @@
-import random, pandas
-
-def getRandomMessage():
-    messages = pandas.read_csv(messages.csv)
-    rand_message = messages['morning'].values(random.randint(0,len(messages)))
-    return rand_message
